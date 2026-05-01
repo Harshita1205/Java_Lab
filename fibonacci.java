79f0b08129fd7ae0c0e65cb3bcd57a33e3080462
Zephyrr <hs12120719@gmail.com>
2026-05-01 20:24:36 +0530

    Add files via upload

--- fibonacci.java
+++ fibonacci.java
@@ -1,22 +1,12 @@
-
-import java.util.Scanner;
-
-public class fibonacci {
+class Fibonacci{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of terms: ");
-        int n = sc.nextInt();
-
-        int a = 0, b = 1;
-
-        System.out.print("Fibonacci Series: " + a + " " + b);
-
-        for (int i = 3; i <= n; i++) {
-            int c = a + b;
-            System.out.print(" " + c);
-            a = b;
-            b = c;
+        int n=10, a=0, b=1;
+        for(int i=0;i<=n;i++)
+        {
+            System.out.println(a+ " ");
+        int c=a+b;
+        a=b;
+        b=c;
         }
     }
 }
